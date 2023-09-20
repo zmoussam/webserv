d3c8728bd82b96b0e1ef774afd4d9bb12b2a4adc
Anas Arbaoui <anas@arbaoui.me>
2023-09-20 14:12:36 +0100

    add: added error handling for 413 content too long

---
+++ www/cookie_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# Import modules for CGI handling 
+import cgi, cgitb , html , os
+# cgitb.enable()
+# parse the HTTP cookies sent by the client
+cookie_string = os.environ.get('HTTP_COOKIE')
+print("cookie_string: ", cookie_string)
+if cookie_string:
+    index = cookie_string.find("=")
+    color = cookie_string[index+1:]
+else:
+    color = "white"
+# access the value of a cookie named "my_cookie"
+# Create instance of FieldStorage 
+form = cgi.FieldStorage()
+
+# Get data from fields
+# Get data from fields
+# if form.getvalue('HTTP_COOKIE'):
+#     color = cookie_string
+if form.getvalue('color') and form.getvalue('color') != color:
+    color = form.getvalue('color')
+
+bodystring = "<html><head><body style=\"background-color: %s;\"></head><body>" % (color)
+bodystring += "<h1>Welcome to my page</h1>"
+bodystring += "<form method=\"GET\" action=\"cookie.py\">"
+bodystring += "<label for=\"color\">Background Color:</label>"
+bodystring += "<input type=\"text\" name=\"color\" id=\"color\">"
+bodystring += "<input type=\"submit\" value=\"Set Color\">"
+bodystring += "</form>"
+bodystring += "</body>"
+bodystring += "</html>"
+print (bodystring)
